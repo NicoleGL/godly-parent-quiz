08400eb2fc2111756cc46a88ca0890d3878aaa16
NicoleGL <nicole.glanga@gmail.com>
2023-06-08 17:56:00 +0200

    Hola

--- Carpeta aleatoria/part1.js
+++ Carpeta aleatoria/part1.js
@@ -1,9 +1,15 @@
-function imprimirString (frase) {
-    console.log(frase);
+
+function imprimirString (palabra) {
+    console.log("Palabra: " + palabra);
+}
+
+async function nombre() {
+    console.log("No me acuerdo de cómo iba javascript");
 }
 
 function sumar (num1, num2) {
-    return num1 + num2;
+    let suma = num1 + num2;
+    return suma;
 }
 
 function gabardina (gab) {
