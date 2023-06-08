3a2f8c9511b1beacec8f48638fe52b6486e27000
NicoleGL <nicole.glanga@gmail.com>
2023-06-08 17:44:50 +0200

    Añadir funciones inutiles

---
+++ Carpeta aleatoria/part1.js
@@ -0,0 +1,17 @@
+function imprimirString (frase) {
+    console.log(frase);
+}
+
+function sumar (num1, num2) {
+    return num1 - num2;
+}
+
+function gabardina (gab) {
+    for(letra in gab) {
+        console.log(letra);
+    }
+}
+
+function meAburro (a) {
+    return [a];
+}
